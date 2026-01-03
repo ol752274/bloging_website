0d9992fa3419f245c0bdbdfef3406a4856eef60f
ol752274 <ol752274@gmail.com>
2026-01-03 21:38:59 +0530

    Posts by category

--- blogs/admin.py
+++ blogs/admin.py
@@ -5,6 +5,6 @@ class BlogAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('title',)}
     list_display = ('title','author','category','status','is_featured','created_at')
     search_fields = ('id','title','category__category_name','status')
-    list_editable = ('is_featured','status')
+    list_editable = ('is_featured',)
 admin.site.register(Category)
 admin.site.register(Blog,BlogAdmin)
--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from django.shortcuts import redirect
+from blogs.models import Blog
+from blogs.models import Category
 # Create your views here.
+def posts_by_category(request, category_id):
+    #Fetch the posts based on category_id
+    posts = Blog.objects.filter(status="Published", category=category_id)
+    try:
+        category = Category.objects.get(pk=category_id)
+    except:
+        return redirect('home')
+    context = {
+        'posts':posts,
+        'category' : category,
+    }
+    return render(request, 'posts_by_category.html', context)
