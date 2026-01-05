992477b11015567cba72690837dc3ba04f8c9793
ol752274 <ol752274@gmail.com>
2026-01-05 08:25:08 +0530

    category functions

---
+++ dashboards/views.py
@@ -0,0 +1,57 @@
+from django.shortcuts import render
+from django.shortcuts import redirect
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.decorators import login_required
+from blogs.models import Category
+from blogs.models import Blog
+from .forms import CategoryForm
+# Create your views here.
+@login_required(login_url='login')
+def dashboard(request):
+    category_count = Category.objects.all().count()
+    blogs_count = Blog.objects.all().count()
+    context = {
+        'category_count': category_count,
+        'blogs_count': blogs_count,
+
+    }
+    return render(request, 'dashboard/dashboard.html',context)
+
+def categories(request):
+    category_count = Category.objects.all().count()
+    blogs_count = Blog.objects.all().count()
+    context = {
+        'category_count': category_count,
+        'blogs_count': blogs_count,
+
+    }
+    return render(request, 'dashboard/categories.html', context) 
+
+def add_category(request):
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('categories')
+    form = CategoryForm()
+    context = {'form': form}
+    return render(request, 'dashboard/add_category.html', context)
+
+def edit_category(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    if request.method == 'POST':
+        form = CategoryForm(request.POST, instance=category)
+        if form.is_valid():
+            form.save()
+            return redirect('categories')
+    form = CategoryForm(instance = category)
+    context = {
+        'form': form,
+        'category': category
+        }
+    return render(request, 'dashboard/edit_category.html', context)
+
+def delete_category(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    category.delete()
+    return redirect('categories')
